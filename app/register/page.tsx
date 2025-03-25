1ddfb65caa9815c6d69c9891d850f13c12881ac4
Maja <majawitosz@gmail.com>
2025-03-25 15:01:57 +0100

    problem eslint

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,8 +5,8 @@ import { Suspense } from 'react';
 
 export default function RegisterPage() {
     return (
-        <main className='flex items-center justify-center md:h-screen'>
-            <div className='relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32'>
+        <main className="flex items-center justify-center md:h-screen">
+            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <Suspense>
                     <RegisterForm />
                 </Suspense>
